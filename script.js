82393f8f20985fe3067013b4a93f7386e36cc4f2
vinjazz <VINJAZZ.VC@GMAIL.COM>
2025-07-07 08:46:09 +0200

    sss

--- script.js
+++ script.js
@@ -1061,6 +1061,7 @@ function handleUserLogin(user) {
 
     // Carica dati utente
     loadUserProfile();
+	initializeNotifications(); 
 
     // Carica lista utenti e notifiche dopo il login
     setTimeout(() => {
