8bbd0b12f7028c87d688a5951a0559227be68253
vinjazz <VINJAZZ.VC@GMAIL.COM>
2025-07-10 15:40:13 +0200

    Remove unused username loading indicator
    Eliminated the username loading spinner element from the HTML and its reference in the JavaScript handler, as it is no longer used in the username save process.

--- HustleCastleCouncil/username_handler.js
+++ HustleCastleCouncil/username_handler.js
@@ -352,7 +352,6 @@ class UsernameManager {
     // Logica interna di salvataggio
     async saveUsernameInternal(username, selectedClan = 'Nessuno', skipUI = false) {
         const saveBtn = document.getElementById('saveUsernameBtn');
-        const loadingEl = document.getElementById('usernameLoading');
         const formEl = document.querySelector('.username-form');
         
         try {
